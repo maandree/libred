cbe3d7d382280ae090ea86b1f415e0f10ea2f408
Mattias Andrée <maandree@kth.se>
2019-10-10 16:55:35 +0200

    Update config.mk
    Signed-off-by: Mattias Andrée <maandree@kth.se>

--- config.mk
+++ config.mk
@@ -2,5 +2,5 @@ PREFIX = /usr
 MANPREFIX = $(PREFIX)/share/man
 
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_GNU_SOURCE
-CFLAGS   = -std=c99 -Wall
-LDFLAGS  = -lm
+CFLAGS   = -std=c99 -Wall -O2
+LDFLAGS  = -lm -s
